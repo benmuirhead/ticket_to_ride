5d9cc9be6aeaef00825fa4e33fd2a8a636ae0ada
Ben Muirhead <bencmuirhead@gmail.com>
2017-02-02 16:39:43 -0500

    Initial R scripts

---
+++ outline.R
@@ -0,0 +1,8 @@
+Outline
+
+Data:
+  Need list of every city
+  need list of every line and distance and color
+  
+Classes:
+  
---
+++ to_do.R
@@ -0,0 +1,11 @@
+# Goals
+# Display a network graph of all cities
+#   make distances accurate
+#   make line colors accurate
+#   lat long accuracy?
+#   lable line distances / segment edges by distance
+# 
+# Route Cards
+#   Be able to visualize route cards
+#   determine requirements for route cards
+#   determine for a given hand if line/route is possible
